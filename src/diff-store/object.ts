b7b6f3d7fbbf11d90782dab4f44e0d157faa28ea
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-07-24 00:47:11 +0100

    feat: Added function to convert diff list to an object diff

---
+++ src/diff-store/object.ts
@@ -0,0 +1,28 @@
+import { Accumulator, diff, Diff } from "deep-diff";
+
+export function difference(lhs: any, rhs: any) {
+  const acc = diff(lhs, rhs, {}, objectAccumulator())
+  return acc['result']
+}
+
+export function objectAccumulator() {
+  return { result: {}, length: 0, push }
+}
+
+export function push(diff: Diff<any>) {
+  // this.result
+  if (diff.path) {
+    setByPath(this.result, diff.path, diff)
+    return
+  }
+  console.log(diff)
+  throw new Error('Unknown diff')
+}
+
+export function setByPath(data: any, path: string[], value: any) {
+  if (path.length === 1) {
+    data[path[0]] = value
+  } else {
+    data[path[0]] = setByPath({}, path.slice(1), value)
+  }
+}
