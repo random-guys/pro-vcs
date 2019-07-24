ea84be89da6f893d758c2b5fc907b2071ff55d73
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-07-24 16:32:35 +0100

    feat: Added method to get the latest request review

--- src/lib/review-request/review-request.repo.ts
+++ src/lib/review-request/review-request.repo.ts
@@ -1,9 +1,23 @@
-import { BaseRepository, MongooseNamespace } from "@random-guys/bucket";
+import { BaseRepository, MongooseNamespace, ModelNotFoundError } from "@random-guys/bucket";
 import { ReviewRequest } from "./review-request.model";
 import { ReviewRequestSchema } from "./review-request.schema";
 
 export class ReviewRequestRepository extends BaseRepository<ReviewRequest>{
   constructor(mongoose: MongooseNamespace) {
     super(mongoose, 'ReviewRequest', ReviewRequestSchema)
+  }
+
+  latestPatch(reference: string, throwOnNull = true) {
+    return new Promise<ReviewRequest>((resolve, reject) => {
+      this.model
+        .findOne({ reference })
+        .sort({ created_at: 'desc' })
+        .exec((err, rev) => {
+          if (err) return reject(err)
+          if (!rev && throwOnNull)
+            return reject(new ModelNotFoundError('ReviewRequest not found'))
+          resolve(rev)
+        })
+    })
   }
 }
