1192243d8725cc5ab3e5b847b9733c49bef35df7
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-10-06 12:21:55 +0100

    fix: Fixed contract export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './event.model';
 export * from './event.repo';
 export * from './event.schema';
 export * from './hub.model';
+export * from './merge.contract';
 export * from './merge.worker';
 export * from './object.util';
 export * from './schema.util';
