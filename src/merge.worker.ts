d12468431420364195c87a9956d49c8dcb7146aa
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-11-25 02:53:14 +0100

    feat: MergeWorker only supports one Repository

--- src/merge.worker.ts
+++ src/merge.worker.ts
@@ -6,16 +6,21 @@ import {
 import {
   createRequestSerializer,
   errSerializer,
-  resSerializer
+  resSerializer,
+  build,
+  Controller,
+  universalErrorHandler,
+  validate
 } from "@random-guys/siber";
 import Logger, { createLogger } from "bunyan";
-import express from "express";
+import express, { Request, Response } from "express";
 import { Server } from "http";
 import mongoose from "mongoose";
-import { WorkerConfig } from "./worker.contract";
-import { ICanMerge } from "./merge.contract";
 import { PayloadModel } from "./event.model";
-import { setupAppRoutes } from "./merge.route";
+import { ICanMerge, Check } from "./merge.contract";
+import { WorkerConfig } from "./worker.contract";
+import { session } from "@random-guys/sp-auth";
+import { isCreateEvent } from "./merge.validator";
 
 /**
  * Configuration for setting up a merge worker
@@ -36,6 +41,7 @@ export class MergeWorker {
   private expressApp = express();
   private httpServer: Server;
   private dbConn: MongooseNamespace;
+  private auth: any;
 
   constructor(private config: MergerConfig) {
     this.logger = createLogger({
@@ -46,6 +52,16 @@ export class MergeWorker {
         req: createRequestSerializer("password")
       }
     });
+
+    build(this.expressApp, this.logger, {
+      cors: false, // we don't need CORS
+      tracking: true
+    });
+
+    this.auth = session({
+      secret: this.config.security_secret,
+      scheme: this.config.security_scheme
+    });
   }
 
   /**
@@ -53,8 +69,6 @@ export class MergeWorker {
    */
   async start<T extends PayloadModel>(merger: ICanMerge<T>) {
     const { app_port, mongodb_url, secure_db, onStart } = this.config;
-    this.httpServer = this.expressApp.listen(app_port);
-    this.logger.info(`🌋 Merger running on port ${app_port}`);
 
     // connect to mongodb if need be
     this.dbConn = await mongoose.connect(
@@ -63,14 +77,80 @@ export class MergeWorker {
     );
     this.logger.info("📦  MongoDB Connected!");
 
-    setupAppRoutes(this.config, this.logger, this.expressApp, merger);
-
     // call user's setup code
     if (onStart) {
       await onStart(this.logger);
     }
 
+    // status checks
+    this.expressApp.get("/", (_req: Request, res: Response) => {
+      res.status(200).json({ status: "UP" });
+    });
+
+    // register 404 route handler
+    this.expressApp.use((req, res, _next) => {
+      res.status(404).send("Whoops! Route doesn't exist.");
+      this.logger.info({ req, res });
+    });
+
+    this.expressApp.use(universalErrorHandler(this.logger));
+
+    this.httpServer = this.expressApp.listen(app_port);
+    this.logger.info(`🌋 Merger running on port ${app_port}`);
+
     process.once("SIGINT", () => this.stop());
+  }
+
+  addMerger<T extends PayloadModel>(route: string, merger: ICanMerge<T>) {
+    // we'll use this to handle errors properly
+    const dummyController = new Controller<T | Check[]>(this.logger);
+
+    /**
+     * Check if an object is valid.
+     */
+    this.expressApp.get(
+      `/${route}/:reference/check`,
+      this.auth.authCheck,
+      async (req, res) => {
+        dummyController.handleSuccess(
+          req,
+          res,
+          await merger.onCheck(req, req.params.reference)
+        );
+      }
+    );
+
+    /**
+     * Approve an object
+     */
+    this.expressApp.post(
+      `/${route}/:reference/approve`,
+      this.auth.authCheck,
+      validate(isCreateEvent),
+      async (req, res) => {
+        dummyController.handleSuccess(
+          req,
+          res,
+          await merger.onApprove(req, req.params.reference, req.body)
+        );
+      }
+    );
+
+    /**
+     * Reject an object
+     */
+    this.expressApp.post(
+      `/${route}/:reference/reject`,
+      this.auth.authCheck,
+      validate(isCreateEvent),
+      async (req, res) => {
+        dummyController.handleSuccess(
+          req,
+          res,
+          await merger.onReject(req, req.params.reference, req.body)
+        );
+      }
+    );
   }
 
   /**
