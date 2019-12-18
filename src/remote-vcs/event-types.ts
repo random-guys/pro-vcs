553bc33fd7f1d5015f02aea1d25eb39bc4c32694
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-12-18 17:37:36 +0100

    feat: how will the remote service know who's sending an event

--- src/remote-vcs/event-types.ts
+++ src/remote-vcs/event-types.ts
@@ -1,38 +1,126 @@
 import { PayloadModel } from "../objects";
 
+/**
+ * An event that signifies that the object was just updated
+ * in an unstable state
+ */
 export interface PatchEvent<T extends PayloadModel> {
+  /**
+   * Event name
+   */
   event_type: "patch";
+  /**
+   * Object reference
+   */
   reference: string;
+  /**
+   * Latest version of the object
+   */
   payload: T;
 }
 
+/**
+ * An event to signify that the object's change or introduction
+ * has been cancelled
+ */
 export interface CloseEvent {
+  /**
+   * Event name
+   */
   event_type: "close";
+  /**
+   * Object reference
+   */
   reference: string;
 }
 
+/**
+ * Signifies that a new object was just added to the repo.
+ */
 export interface NewObjectEvent<T extends PayloadModel> {
+  /**
+   * Event name
+   */
   event_type: "create.new";
+  /**
+   * The namespace of the repo
+   */
+  namespace: string;
+  /**
+   * Object reference
+   */
   reference: string;
+  /**
+   * Initiator of the process
+   */
   owner: string;
+  /**
+   * The new object
+   */
   payload: T;
 }
 
+/**
+ * Signifies that an update has happened on a pre-exisiting
+ * stable object.
+ */
 export interface UpdateObjectEvent<T extends PayloadModel> {
+  /**
+   * Event name
+   */
   event_type: "create.update";
+  /**
+   * The namespace of the repo
+   */
+  namespace: string;
+  /**
+   * Object reference
+   */
   reference: string;
+  /**
+   * Initiator of the process
+   */
   owner: string;
+  /**
+   * The initial object before the change
+   */
   payload: T;
+  /**
+   * The changes applied
+   */
   update: Partial<T>;
 }
 
+/**
+ * Signifies that a user wants to delete a stable object
+ */
 export interface DeleteObjectEvent<T extends PayloadModel> {
+  /**
+   * Event name
+   */
   event_type: "create.delete";
+  /**
+   * The namespace of the repo
+   */
+  namespace: string;
+  /**
+   * Object reference
+   */
   reference: string;
+  /**
+   * Initiator of the process
+   */
   owner: string;
+  /**
+   * I honestly don't know why I added this.
+   */
   payload: T;
 }
 
+/**
+ * This is any event that leads to the creation of a
+ * review request.
+ */
 export type CreateEvent<T extends PayloadModel> =
   | NewObjectEvent<T>
   | UpdateObjectEvent<T>
